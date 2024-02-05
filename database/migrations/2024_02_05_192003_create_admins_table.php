9d4fff4f5dd8a7584487815e4a63fed8027121fb
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-06 05:16:53 +0800

    Progress: Make Migration & Seeder Admin

--- database/migrations/2024_02_05_192003_create_admins_table.php
+++ database/migrations/2024_02_05_192003_create_admins_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
             $table->bigInteger('users_id')->index();
             $table->string('name', 255);
             $table->string('email', 255)->unique();
-            $table->string('password', 13);
+            $table->string('password', 100);
             $table->string('image');
             $table->string('phone_number', 13);
             $table->date('date_of_birth');
-            $table->date('place_of_birth');
+            $table->string('place_of_birth');
             $table->timestamps();
         });
     }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Admin::create([
+            'users_id' => 1,
+            'name' => 'Ivan Verdyansyah',
+            'email' => 'ivanverdyansyah@gmail.com',
+            'password' => bcrypt('ivan123'),
+            'image' => 'profile-3.jpg',
+            'phone_number' => '081238484005',
+            'date_of_birth' => '2005-03-09',
+            'place_of_birth' => 'Denpasar',
+        ]);
     }
 }
