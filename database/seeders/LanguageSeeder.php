e9b4daf7234ae27a3f01bb913d1726beaf4263be
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-06 05:17:50 +0800

    Progress: Make Seeder Language

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Language;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class LanguageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Language::create([
+            'name' => 'Indonesian',
+        ]);
+
+        Language::create([
+            'name' => 'England',
+        ]);
+
+        Language::create([
+            'name' => 'Swiss',
+        ]);
     }
 }
