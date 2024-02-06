61a4d9ce0c873d49b3f1304dab948cf79a1c21b9
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 06:10:00 +0800

    Progress: Finish Page Index Region

---
+++ resources/views/region/index.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.main')
+
+@section('content-dashboard')
+<div class="container-fluid">
+    <div class="card w-100">
+        <div class="card-body px-3 px-md-4">
+            <div class="wrapper d-flex align-items-center justify-content-between mb-3">
+                <h5 class="card-title fw-semibold mb-0 d-none d-md-inline-block">Data All Region</h5>
+                <button type="button" class="btn btn-primary d-flex align-items-center gap-1" data-bs-toggle="modal" data-bs-target="#createModal">
+                    <span>Add Region</span>
+                    <i class="ti ti-circle-plus"></i>
+                </button>
+            </div>
+            @if (session()->has('success'))
+                <div class="alert alert-success w-100 mb-4" role="alert">
+                    {{ session('success') }}
+                </div>
+            @elseif(session()->has('failed'))
+                <div class="alert alert-danger w-100 mb-4" role="alert">
+                    {{ session('failed') }}
+                </div>
+            @endif
+            <div class="table-responsive">
+                <table class="table text-nowrap mb-0 align-middle">
+                    <thead class="text-dark fs-4 border-bottom">
+                        <tr>
+                            <th class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0">#</h6>
+                            </th>
+                            <th class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0">Name</h6>
+                            </th>
+                            <th class="border-bottom-0">
+                                <h6 class="fw-semibold mb-0">Description</h6>
+                            </th>
+                            <th class="border-bottom-0" style="width:180px !important;">
+                                <h6 class="fw-semibold mb-0"></h6>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($regions->count() == 0)
+                            <tr>
+                                <td>Data Region Not Found!</td>
+                            </tr>
+                        @else
+                            @foreach ($regions as $i => $region)
+                                <tr>
+                                    <td class="border-bottom-0">
+                                        <h6 class="mb-0 fw-normal">{{ $i + 1 }}</h6>
+                                    </td>
+                                    <td class="border-bottom-0">
+                                        <h6 class="mb-0 fw-normal">{{ $region->name }}</h6>
+                                    </td>
+                                    <td class="border-bottom-0">
+                                        <p class="mb-0 fw-normal">{{ $region->description }}</p>
+                                    </td>
+                                    <td class="border-bottom-0" style="width:180px !important;">
+                                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#detailModal" data-id="{{ $region->id }}">
+                                            <span>
+                                                <i class="ti ti-id"></i>
+                                            </span>
+                                        </button>
+                                        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editModal" data-id="{{ $region->id }}">
+                                            <span>
+                                                <i class="ti ti-edit"></i>
+                                            </span>
+                                        </button>
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" data-id="{{ $region->id }}">
+                                            <span>
+                                                <i class="ti ti-trash"></i>
+                                            </span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @endif
+                    </tbody>
+                    {{-- <tbody>
+                        <tr>
+                            <td class="border-bottom-0 bg-danger">
+                                {{ $regions->links() }}
+                            </td>
+                        </tr>
+                    </tbody> --}}
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('partials.region')
+@push('js')
+    <script>
+        $(document).on('click', '[data-bs-target="#detailModal"]', function() {
+            let id = $(this).data('id');
+            $.ajax({
+                type: 'get',
+                url: '/region/detail/' + id,
+                success: function(data) {
+                    $('[data-value="name"]').val(data.name);
+                    $('[data-value="description"]').html(data.description);
+                }
+            });
+        });
+
+        $(document).on('click', '[data-bs-target="#editModal"]', function() {
+            let id = $(this).data('id');
+            $('#buttonEditRegion').attr('action', '/region/edit/' + id);
+            $.ajax({
+                type: 'get',
+                url: '/region/detail/' + id,
+                success: function(data) {
+                    $('[data-value="name"]').val(data.name);
+                    $('[data-value="description"]').html(data.description);
+                }
+            });
+        });
+
+        $(document).on('click', '[data-bs-target="#deleteModal"]', function() {
+            let id = $(this).data('id');
+            $('#buttonDeleteRegion').attr('action', '/region/delete/' + id);
+        });
+    </script>
+@endpush
+@endsection
