2c2951f010826589c6a813baec384d269804f49e
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 04:15:11 +0800

    Progress: Finish Model Staff

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Staff extends Model
 {
-    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'users_id');
+    }
 }
