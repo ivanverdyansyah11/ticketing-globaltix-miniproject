6b59f34dcc36902a08b105f0803692e37edb46f1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 04:17:07 +0800

    Progress: Finish Migration Seeder Admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,15 +13,23 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // Admin::create([
+        //     'users_id' => 1,
+        //     'name' => 'Ivan Verdyansyah',
+        //     'email' => 'ivanverdyansyah@gmail.com',
+        //     'password' => bcrypt('ivan123'),
+        //     'image' => 'profile-3.jpg',
+        //     'phone_number' => '081238484005',
+        //     'date_of_birth' => '2005-03-09',
+        //     'place_of_birth' => 'Denpasar',
+        // ]);
+
         Admin::create([
-            'users_id' => 1,
-            'name' => 'Ivan Verdyansyah',
-            'email' => 'ivanverdyansyah@gmail.com',
-            'password' => bcrypt('ivan123'),
-            'image' => 'profile-3.jpg',
-            'phone_number' => '081238484005',
-            'date_of_birth' => '2005-03-09',
-            'place_of_birth' => 'Denpasar',
+            'users_id' => 10,
+            'name' => 'Bastian Steel',
+            'phone_number' => '087685675656',
+            'date_of_birth' => '2002-05-23',
+            'place_of_birth' => 'Badung',
         ]);
     }
 }
