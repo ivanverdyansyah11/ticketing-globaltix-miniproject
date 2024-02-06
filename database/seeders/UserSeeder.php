cc187e09bb7821b2527d7eb0641895c3ffcbaf8c
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 04:17:41 +0800

    Progress: Finish Migration Seeder User

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,55 +16,71 @@ class UserSeeder extends Seeder
         User::create([
             'email' => 'ivanverdyansyah@gmail.com',
             'password' => bcrypt('ivan123'),
-            'role' => 'admin',
+            'image' => 'profile-1.jpg',
+            'role' => 'super admin',
         ]);
 
         User::create([
             'email' => 'adityaprayatna@gmail.com',
             'password' => bcrypt('aditya123'),
+            'image' => 'profile-1.jpg',
             'role' => 'staff',
         ]);
 
         User::create([
             'email' => 'ayuridiantari@gmail.com',
             'password' => bcrypt('ayu123'),
+            'image' => 'profile-1.jpg',
             'role' => 'staff',
         ]);
 
         User::create([
             'email' => 'aguspratama@gmail.com',
             'password' => bcrypt('agus123'),
+            'image' => 'profile-1.jpg',
             'role' => 'tourguide',
         ]);
 
         User::create([
             'email' => 'devinaputri@gmail.com',
             'password' => bcrypt('devina123'),
+            'image' => 'profile-1.jpg',
             'role' => 'tourguide',
         ]);
 
         User::create([
             'email' => 'putrahasan@gmail.com',
             'password' => bcrypt('putra123'),
+            'image' => 'profile-1.jpg',
             'role' => 'tourguide',
         ]);
 
         User::create([
             'email' => 'yogakusuma@gmail.com',
             'password' => bcrypt('yoga123'),
+            'image' => 'profile-1.jpg',
             'role' => 'customer',
         ]);
 
         User::create([
             'email' => 'krisnahartawan@gmail.com',
             'password' => bcrypt('krisna123'),
+            'image' => 'profile-1.jpg',
             'role' => 'customer',
         ]);
 
         User::create([
             'email' => 'aliciasyabana@gmail.com',
             'password' => bcrypt('alicia123'),
+            'image' => 'profile-1.jpg',
             'role' => 'customer',
+        ]);
+
+        User::create([
+            'email' => 'bastiansteel@gmail.com',
+            'password' => bcrypt('bastian123'),
+            'image' => 'profile-1.jpg',
+            'role' => 'admin',
         ]);
     }
 }
