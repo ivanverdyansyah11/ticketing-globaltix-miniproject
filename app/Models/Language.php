995e7b25e71e7661844989165bf945e2b14e66df
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 04:15:04 +0800

    Progress: Finish Model Language

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Language extends Model
 {
-    use HasFactory;
+    protected $guarded = ['id'];
+
+    // public function tourguide() {
+    //     return $this->belongsTo(TourGuide::class, 'id');
+    // }
 }
