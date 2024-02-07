fd9f458d96be3d1c4eac75e52a1ad9b3bc82b782
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 02:46:31 +0800

    Progress: Finish Model Facility

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Facility extends Model
 {
     protected $guarded = ['id'];
+
+    public function touristsitefacility() {
+        return $this->hasMany(TouristSiteFacility::class, 'id');
+    }
 }
