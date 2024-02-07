d0d2041d713d69d471d47f36cc90f1547b5d7e8c
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 14:40:22 +0800

    Progress: Finish Model Region Category

--- app/Models/RegionCategory.php
+++ app/Models/RegionCategory.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegionCategory extends Model
 {
-    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class, 'categories_id');
+    }
+
+    public function region() {
+        return $this->belongsTo(Region::class, 'regions_id');
+    }
 }
