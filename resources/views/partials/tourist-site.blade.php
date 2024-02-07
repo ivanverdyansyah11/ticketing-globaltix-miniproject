0c1c92bace23fa2f921b089f418fd3f758eb2b82
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 22:44:47 +0800

    Progress: Add Partial Modal Tourist Site

---
+++ resources/views/partials/tourist-site.blade.php
@@ -0,0 +1,140 @@
+<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="createModalLabel">Modal Add New Tourist Site</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="{{ route('toursite.store') }}" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="region_categories_id" class="form-label">Region</label>
+                        <select name="region_categories_id" class="form-control @error('region_categories_id') is-invalid @enderror" id="region_categories_id">
+                            <option value="-">Choose region category</option>
+                            @foreach ($region_catergories as $region_catergory)
+                                <option value="{{ $region_catergory->region->id }}">{{ $region_catergory->region->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('region_categories_id')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input required name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-0">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea required name="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="4"></textarea>
+                        @error('description')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Add</button>
+                    <button type="submit" class="btn btn-primary">Add New Tourist Site</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="detailModalLabel">Modal Detail Tourist Site</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="d-inline-block w-100">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="region_categories_id" class="form-label">Region</label>
+                        <input readonly name="region_categories_id" type="text" class="form-control" id="region_categories_id" data-value="region_categories_id">
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input readonly name="name" type="text" class="form-control" id="name" data-value="name">
+                    </div>
+                    <div class="mb-0">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea readonly name="description" class="form-control" id="description" rows="4" data-value="description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close Modal Detail</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Modal Edit Tourist Site</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonEditTouristSite" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="region_categories_id" class="form-label">Region</label>
+                        <select required name="region_categories_id" class="form-control @error('region_categories_id') is-invalid @enderror" id="region_categories_id" data-element="row-edit-select">
+                        </select>
+                        @error('region_categories_id')
+                          <div class="invalid-feedback">
+                            {{ $message }}
+                          </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input name="name" type="text" class="form-control" id="name" data-value="name">
+                    </div>
+                    <div class="mb-0">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea name="description" class="form-control" id="description" rows="4" data-value="description"></textarea>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Edit</button>
+                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Delete Tourist Site</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonDeleteTouristSite" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <p>Are your sure want to delete this tourist site?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
+                    <button type="submit" class="btn btn-primary">Delete Tourist Site</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
