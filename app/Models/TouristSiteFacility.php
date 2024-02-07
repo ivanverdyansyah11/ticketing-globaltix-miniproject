4c00a9da47814a4d40f714eba236e4ce6e7b1f78
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 02:47:51 +0800

    Progress: Add Relation with Model Tourist Site & Facility Model Tourist Site Facility

--- app/Models/TouristSiteFacility.php
+++ app/Models/TouristSiteFacility.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class TouristSiteFacility extends Model
 {
-    use HasFactory;
+    protected $guarded = ['id'];
+
+    public function touristsite() {
+        return $this->belongsTo(TouristSite::class, 'tourist_sites_id');
+    }
+
+    public function facility() {
+        return $this->belongsTo(Facility::class, 'facilities_id');
+    }
 }
