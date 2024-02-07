2ab59b0f8f67a976d04f735a1bee71082e495cb2
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 02:48:33 +0800

    Progress: Revision Seeder Tourist Site Facility

--- database/seeders/TouristSiteFacilitySeeder.php
+++ database/seeders/TouristSiteFacilitySeeder.php
@@ -15,42 +15,42 @@ class TouristSiteFacilitySeeder extends Seeder
     {
         TouristSiteFacility::create([
             'tourist_sites_id' => 1,
-            'facilities_id' => 1,
+            'facilities_id' => '1,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 2,
-            'facilities_id' => 2,
+            'facilities_id' => '2,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 3,
-            'facilities_id' => 1,
+            'facilities_id' => '1,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 3,
-            'facilities_id' => 2,
+            'facilities_id' => '2,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 3,
-            'facilities_id' => 3,
+            'facilities_id' => '3,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 4,
-            'facilities_id' => 4,
+            'facilities_id' => '4,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 5,
-            'facilities_id' => 3,
+            'facilities_id' => '3,5',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 5,
-            'facilities_id' => 5,
+            'facilities_id' => '1,5',
         ]);
     }
 }
