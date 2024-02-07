94c4444b0b04a8534d0044fcdeb2ac69c6aa41f1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 14:39:54 +0800

    Progress: Finish Model Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = ['id'];
+
+    public function regionCategory() {
+        return $this->hasMany(RegionCategory::class, 'id');
+    }
 }
