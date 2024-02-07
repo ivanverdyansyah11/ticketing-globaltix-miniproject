a2246f3adc817f7b327b7ed8e799794c493655ad
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-07 14:40:51 +0800

    Progress: Revision Seeder Region Category

--- database/seeders/RegionCategorySeeder.php
+++ database/seeders/RegionCategorySeeder.php
@@ -15,27 +15,27 @@ class RegionCategorySeeder extends Seeder
     {
         RegionCategory::create([
             'regions_id' => 1,
-            'categories_id' => '[1, 2]',
+            'categories_id' => '1,2',
         ]);
 
         RegionCategory::create([
             'regions_id' => 2,
-            'categories_id' => '[2, 3]',
+            'categories_id' => '2,3',
         ]);
 
         RegionCategory::create([
             'regions_id' => 3,
-            'categories_id' => '[3, 4]',
+            'categories_id' => '3,4',
         ]);
 
         RegionCategory::create([
             'regions_id' => 4,
-            'categories_id' => '[4, 5]',
+            'categories_id' => '4,5',
         ]);
 
         RegionCategory::create([
             'regions_id' => 5,
-            'categories_id' => '[1, 5]',
+            'categories_id' => '1,5',
         ]);
     }
 }
