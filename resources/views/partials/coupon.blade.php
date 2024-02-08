9681eced0b9b13c25141b423a9fe7df423ffe308
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 08:35:43 +0800

    Progress: Update Add Partial Modal Coupon

---
+++ resources/views/partials/coupon.blade.php
@@ -0,0 +1,166 @@
+<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="createModalLabel">Modal Add New Coupon</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="{{ route('coupon.store') }}" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="coupon_code" class="form-label">Coupon Code</label>
+                        <input required name="coupon_code" type="text" class="form-control @error('coupon_code') is-invalid @enderror" id="coupon_code">
+                        @error('coupon_code')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="discount_percentage" class="form-label">Discount Percentage</label>
+                        <input required name="discount_percentage" type="number" class="form-control @error('discount_percentage') is-invalid @enderror" id="discount_percentage">
+                        @error('discount_percentage')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="start_date" class="form-label">Start Date</label>
+                        <input required name="start_date" type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date">
+                        @error('start_date')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-0">
+                        <label for="expired_date" class="form-label">Expired Date</label>
+                        <input required name="expired_date" type="date" class="form-control @error('expired_date') is-invalid @enderror" id="expired_date">
+                        @error('expired_date')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Add</button>
+                    <button type="submit" class="btn btn-primary">Add New Coupon</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="detailModalLabel">Modal Detail Coupon</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="d-inline-block w-100">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="coupon_code" class="form-label">Coupon Code</label>
+                        <input readonly name="coupon_code" type="text" class="form-control" id="coupon_code" data-value="coupon_code">
+                    </div>
+                    <div class="mb-3">
+                        <label for="discount_percentage" class="form-label">Discount Percentage</label>
+                        <input readonly name="discount_percentage" type="number" class="form-control" id="discount_percentage" data-value="discount_percentage">
+                    </div>
+                    <div class="mb-3">
+                        <label for="start_date" class="form-label">Start Date</label>
+                        <input readonly name="start_date" type="date" class="form-control" id="start_date" data-value="start_date">
+                    </div>
+                    <div class="mb-0">
+                        <label for="expired_date" class="form-label">Expired Date</label>
+                        <input readonly name="expired_date" type="date" class="form-control" id="expired_date" data-value="expired_date">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close Modal Detail</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Modal Edit Coupon</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonEditCoupon" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="coupon_code" class="form-label">Coupon Code</label>
+                        <input required name="coupon_code" type="text" class="form-control @error('coupon_code') is-invalid @enderror" id="coupon_code" data-value="coupon_code">
+                        @error('coupon_code')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="discount_percentage" class="form-label">Discount Percentage</label>
+                        <input required name="discount_percentage" type="number" class="form-control @error('discount_percentage') is-invalid @enderror" id="discount_percentage" data-value="discount_percentage">
+                        @error('discount_percentage')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="start_date" class="form-label">Start Date</label>
+                        <input required name="start_date" type="date" class="form-control @error('start_date') is-invalid @enderror" id="start_date" data-value="start_date">
+                        @error('start_date')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="mb-0">
+                        <label for="expired_date" class="form-label">Expired Date</label>
+                        <input required name="expired_date" type="date" class="form-control @error('expired_date') is-invalid @enderror" id="expired_date" data-value="expired_date">
+                        @error('expired_date')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Edit</button>
+                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Delete Coupon</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonDeleteCoupon" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <p>Are your sure want to delete this coupon?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
+                    <button type="submit" class="btn btn-primary">Delete Coupon</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
