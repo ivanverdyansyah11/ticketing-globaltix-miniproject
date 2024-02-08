d64f6d966b110981bb338a2bbe5a9ef2729ded34
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 08:32:22 +0800

    Progress: Revision Data Seeder Region

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -14,28 +14,28 @@ class RegionSeeder extends Seeder
     public function run(): void
     {
         Region::create([
-            'name' => 'Cityville',
-            'description' => 'Bustling urban center known for its culture',
+            'name' => 'Bali Bliss',
+            'description' => 'Discover the enchanting beauty of Bali Bliss, a tropical paradise known for its vibrant culture, stunning beaches, and lush rice terraces.',
         ]);
 
         Region::create([
-            'name' => 'Coastal Haven',
-            'description' => 'Serene coastal area with beautiful beaches',
+            'name' => 'Komodo Kingdom',
+            'description' => 'Embark on a journey to Komodo Kingdom, home of the legendary Komodo dragons, where pristine beaches and diverse marine life await.',
         ]);
 
         Region::create([
-            'name' => 'Mountain Retreat',
-            'description' => 'Scenic mountainous region perfect for hiking',
+            'name' => 'Java Jungle Trek',
+            'description' => "Explore the wild side of Java Jungle Trek, an adventure-filled destination boasting ancient temples, dense rainforests, and active volcanoes.",
         ]);
 
         Region::create([
-            'name' => 'Historical Hamlet',
-            'description' => 'Quaint town rich in history and charm',
+            'name' => 'Raja Ampat Wonders',
+            'description' => 'Dive into the underwater wonders of Raja Ampat, a marine paradise with crystal-clear waters, vibrant coral reefs, and exotic marine species.',
         ]);
 
         Region::create([
-            'name' => 'Countryside Oasis',
-            'description' => 'Tranquil countryside with rolling landscapes',
+            'name' => 'Yogyakarta Heritage',
+            'description' => 'Immerse yourself in the rich heritage of Yogyakarta, a city of ancient temples, royal palaces, and traditional arts, nestled in the heart of Java.',
         ]);
     }
 }
