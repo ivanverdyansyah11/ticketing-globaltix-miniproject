1507ab016c161642c96b8bdfc131b641733c50f1
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 08:27:35 +0800

    Progress: Finish Store Request Ticket

---
+++ app/Http/Requests/StoreTicketRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tourist_site_facilities_id' => 'required|integer',
+            'ticket_categories_id' => 'required|integer',
+            'price' => 'required|integer',
+            'stock_per_day' => 'required|integer',
+        ];
+    }
+}
