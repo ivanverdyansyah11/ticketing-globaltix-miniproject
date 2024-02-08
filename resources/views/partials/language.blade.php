07ee7c46beb2a17317fbc4402d24d969d4103527
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 16:09:14 +0800

    Progress: Finish Add Partial Modal Language

---
+++ resources/views/partials/language.blade.php
@@ -0,0 +1,100 @@
+<div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="createModalLabel">Modal Add New Language</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="{{ route('language.store') }}" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-0">
+                        <label for="name" class="form-label">Name</label>
+                        <input required name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Add</button>
+                    <button type="submit" class="btn btn-primary">Add New Language</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="detailModalLabel">Modal Detail Language</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form class="d-inline-block w-100">
+                <div class="modal-body">
+                    <div class="mb-0">
+                        <label for="name" class="form-label">Name</label>
+                        <input readonly name="name" type="text" class="form-control" id="name" data-value="name">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Close Modal Detail</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="editModalLabel">Modal Edit Language</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonEditLanguage" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-0">
+                        <label for="name" class="form-label">Name</label>
+                        <input name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" data-value="name">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Edit</button>
+                    <button type="submit" class="btn btn-primary">Save Changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="deleteModalLabel">Modal Delete Language</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form id="buttonDeleteLanguage" method="POST" class="d-inline-block w-100">
+                @csrf
+                <div class="modal-body">
+                    <p>Are your sure want to delete this language?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-bs-dismiss="modal">Cancel Delete</button>
+                    <button type="submit" class="btn btn-primary">Delete Language</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
