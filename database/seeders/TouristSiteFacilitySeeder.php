0e21c00208b1ddc910781b9826f92457558a3c9a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-08 08:33:10 +0800

    Progress: Revision Data Seeder Tourist Site Facility

--- database/seeders/TouristSiteFacilitySeeder.php
+++ database/seeders/TouristSiteFacilitySeeder.php
@@ -15,42 +15,52 @@ class TouristSiteFacilitySeeder extends Seeder
     {
         TouristSiteFacility::create([
             'tourist_sites_id' => 1,
-            'facilities_id' => '1,5',
+            'facilities_id' => '1,2',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 2,
-            'facilities_id' => '2,5',
+            'facilities_id' => '2,3',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 3,
-            'facilities_id' => '1,5',
-        ]);
-
-        TouristSiteFacility::create([
-            'tourist_sites_id' => 3,
-            'facilities_id' => '2,5',
-        ]);
-
-        TouristSiteFacility::create([
-            'tourist_sites_id' => 3,
-            'facilities_id' => '3,5',
+            'facilities_id' => '1,3',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 4,
-            'facilities_id' => '4,5',
-        ]);
-
-        TouristSiteFacility::create([
-            'tourist_sites_id' => 5,
-            'facilities_id' => '3,5',
+            'facilities_id' => '2,4',
         ]);
 
         TouristSiteFacility::create([
             'tourist_sites_id' => 5,
             'facilities_id' => '1,5',
+        ]);
+
+        TouristSiteFacility::create([
+            'tourist_sites_id' => 6,
+            'facilities_id' => '1,3',
+        ]);
+
+        TouristSiteFacility::create([
+            'tourist_sites_id' => 7,
+            'facilities_id' => '4,5',
+        ]);
+
+        TouristSiteFacility::create([
+            'tourist_sites_id' => 8,
+            'facilities_id' => '2,3',
+        ]);
+
+        TouristSiteFacility::create([
+            'tourist_sites_id' => 9,
+            'facilities_id' => '1,5',
+        ]);
+
+        TouristSiteFacility::create([
+            'tourist_sites_id' => 10,
+            'facilities_id' => '1,4',
         ]);
     }
 }
