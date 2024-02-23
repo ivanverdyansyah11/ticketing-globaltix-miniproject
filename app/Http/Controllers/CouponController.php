12c3ffd0fd8acbcef9dbe8c374d3ed2b8ab9e985
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:15:37 +0800

    Progress: Update Clean Code Coupon Controller

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCouponRequest;
 use App\Http\Requests\UpdateCouponRequest;
 use App\Models\Coupon;
+use App\Repositories\CouponRepositories;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
@@ -12,60 +13,55 @@ use Illuminate\Http\Request;
 
 class CouponController extends Controller
 {
+    public function __construct(
+        protected readonly CouponRepositories $coupon,
+    ) {}
+
     public function index() : View {
         return view('coupon.index', [
             'title' => 'Coupon Page',
-            'coupons' => Coupon::orderBy('created_at', 'DESC')->paginate(10),
+            'coupons' => $this->coupon->findAllPaginate(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $coupon = Coupon::where('id', $id)->first();
+    public function show(Coupon $coupon) : JsonResponse {
         try {
             return response()->json([
                 'status' => 'success',
-                'data' => $coupon,
+                'data' => $this->coupon->findById($coupon->id),
             ]);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Coupon with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Coupon with ID ' . $coupon->id,
+            ], 500);
         }
     }
 
     public function store(StoreCouponRequest $request) : RedirectResponse {
         try {
-            Coupon::create($request->all());
-
-            return redirect(route('coupon'))->with('success', 'Successfully Add New Coupon!');
+            $this->coupon->store($request->validated());
+            return redirect(route('coupon.index'))->with('success', 'Successfully Add New Coupon!');
         } catch (\Throwable $th) {
-            return redirect(route('coupon'))->with('failed', 'Failed Add New Coupon!');
+            return redirect(route('coupon.index'))->with('failed', 'Failed Add New Coupon!');
         }
     }
 
-    public function update(UpdateCouponRequest $request, $id) : RedirectResponse {
-        $coupon = Coupon::where('id', $id)->first();
-
+    public function update(UpdateCouponRequest $request, Coupon $coupon) : RedirectResponse {
         try {
-            $coupon->update($request->all());
-
-            return redirect(route('coupon'))->with('success', 'Successfully Update Coupon!');
-
+            $this->coupon->update($request->validated(), $coupon);
+            return redirect(route('coupon.index'))->with('success', 'Successfully Update Coupon!');
         } catch (\Throwable $th) {
-            return redirect(route('coupon'))->with('failed', 'Failed Update Coupon!');
+            return redirect(route('coupon.index'))->with('failed', 'Failed Update Coupon!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $coupon = Coupon::where('id', $id)->first();
-
+    public function destroy(Coupon $coupon) : RedirectResponse {
         try {
-            $coupon->delete();
-
-            return redirect(route('coupon'))->with('success', 'Successfully Delete Coupon!');
+            $this->coupon->delete($coupon);
+            return redirect(route('coupon.index'))->with('success', 'Successfully Delete Coupon!');
         } catch (\Throwable $th) {
-            return redirect(route('coupon'))->with('failde', 'Failed Delete Coupon!');
+            return redirect(route('coupon.index'))->with('failde', 'Failed Delete Coupon!');
         }
     }
 }
