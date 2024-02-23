482c109af0b0fb40a8a31717dfd8a14fe94b62a8
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:16:57 +0800

    Progress: Update Clean Code Tourist Site Controller

--- app/Http/Controllers/TouristSiteController.php
+++ app/Http/Controllers/TouristSiteController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreTouristSiteRequest;
 use App\Http\Requests\UpdateTouristSiteRequest;
 use App\Models\RegionCategory;
 use App\Models\TouristSite;
+use App\Repositories\RegionCategoryRepositories;
+use App\Repositories\TouristSiteRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,17 +15,22 @@ use Illuminate\View\View;
 
 class TouristSiteController extends Controller
 {
+    public function __construct(
+        protected readonly TouristSiteRepositories $touristsite,
+        protected readonly RegionCategoryRepositories $regionCategory,
+    ) {}
+
     public function index() : View {
         return view('tourist-site.index', [
             'title' => 'Tourist Site Page',
-            'tourist_sites' => TouristSite::orderBy('created_at', 'DESC')->paginate(10),
-            'region_catergories' => RegionCategory::with(['region'])->get(),
+            'tourist_sites' => $this->touristsite->findAllPaginate(),
+            'region_catergories' => $this->regionCategory->findAll(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $tourist_site = TouristSite::with(['regioncategory.region'])->where('id', $id)->first();
-        $regionCategory = RegionCategory::with(['region', 'category'])->get();
+    public function show(TouristSite $touristsite) : JsonResponse {
+        $tourist_site = $this->touristsite->findById($touristsite->id);
+        $regionCategory = $this->regionCategory->findAll();
         try {
             return response()->json([
                 'status' => 'success',
@@ -32,43 +39,35 @@ class TouristSiteController extends Controller
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Tourist Site with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Tourist Site with ID ' . $touristsite->id,
+            ], 500);
         }
     }
 
     public function store(StoreTouristSiteRequest $request) : RedirectResponse {
         try {
-            TouristSite::create($request->all());
-
-            return redirect(route('toursite'))->with('success', 'Successfully Add New Tourist Site!');
+            $this->touristsite->store($request->validated());
+            return redirect(route('touristsite.index'))->with('success', 'Successfully Add New Tourist Site!');
         } catch (\Throwable $th) {
-            return redirect(route('toursite'))->with('failed', 'Failed Add New Tourist Site!');
+            return redirect(route('touristsite.index'))->with('failed', 'Failed Add New Tourist Site!');
         }
     }
 
-    public function update(UpdateTouristSiteRequest $request, $id) : RedirectResponse {
-        $tourist_site = TouristSite::where('id', $id)->first();
-
+    public function update(UpdateTouristSiteRequest $request, TouristSite $touristsite) : RedirectResponse {
         try {
-            $tourist_site->update($request->all());
-
-            return redirect(route('toursite'))->with('success', 'Successfully Update Tourist Site!');
-
+            $this->touristsite->update($request->validated(), $touristsite);
+            return redirect(route('touristsite.index'))->with('success', 'Successfully Update Tourist Site!');
         } catch (\Throwable $th) {
-            return redirect(route('toursite'))->with('failed', 'Failed Update Tourist Site!');
+            return redirect(route('touristsite.index'))->with('failed', 'Failed Update Tourist Site!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $tourist_site = TouristSite::where('id', $id)->first();
-
+    public function destroy(TouristSite $touristsite) : RedirectResponse {
         try {
-            $tourist_site->delete();
-
-            return redirect(route('toursite'))->with('success', 'Successfully Delete Tourist Site!');
+            $this->touristsite->delete($touristsite);
+            return redirect(route('touristsite.index'))->with('success', 'Successfully Delete Tourist Site!');
         } catch (\Throwable $th) {
-            return redirect(route('toursite'))->with('failde', 'Failed Delete Tourist Site!');
+            return redirect(route('touristsite.index'))->with('failde', 'Failed Delete Tourist Site!');
         }
     }
 }
