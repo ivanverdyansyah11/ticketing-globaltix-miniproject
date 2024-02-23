666b5dc5d11097755e2b13f70bea2cfa335b69dd
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:16:02 +0800

    Progress: Update Clean Code Facility Controller

--- app/Http/Controllers/FacilityController.php
+++ app/Http/Controllers/FacilityController.php
@@ -5,67 +5,62 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreFacilityRequest;
 use App\Http\Requests\UpdateFacilityRequest;
 use App\Models\Facility;
+use App\Repositories\FacilityRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class FacilityController extends Controller
 {
+    public function __construct(
+        protected readonly FacilityRepositories $facility,
+    ) {}
+
     public function index() : View {
         return view('facility.index', [
             'title' => 'Facility Page',
-            'facilities' => Facility::orderBy('created_at', 'DESC')->paginate(10),
+            'facilities' => $this->facility->findAllPaginate(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $facility = Facility::where('id', $id)->first();
+    public function show(Facility $facility) : JsonResponse {
         try {
             return response()->json([
                 'status' => 'success',
-                'data' => $facility,
+                'data' => $this->facility->findById($facility->id),
             ]);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Facility with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Facility with ID ' . $facility->id,
+            ], 500);
         }
     }
 
     public function store(StoreFacilityRequest $request) : RedirectResponse {
         try {
-            Facility::create($request->all());
-
-            return redirect(route('facility'))->with('success', 'Successfully Add New Facility!');
+            $this->facility->store($request->validated());
+            return redirect(route('facility.index'))->with('success', 'Successfully Add New Facility!');
         } catch (\Throwable $th) {
-            return redirect(route('facility'))->with('failed', 'Failed Add New Facility!');
+            return redirect(route('facility.index'))->with('failed', 'Failed Add New Facility!');
         }
     }
 
-    public function update(UpdateFacilityRequest $request, $id) : RedirectResponse {
-        $facility = Facility::where('id', $id)->first();
-
-        try {
-            $facility->update($request->all());
-
-            return redirect(route('facility'))->with('success', 'Successfully Update Facility!');
-
-        } catch (\Throwable $th) {
-            return redirect(route('facility'))->with('failed', 'Failed Update Facility!');
+    public function update(UpdateFacilityRequest $request, Facility $facility) : RedirectResponse {
+        try {                     
+            $this->facility->update($request->validated(), $facility);
+            return redirect(route('facility.index'))->with('success', 'Successfully Update Facility!');
+        } catch (\Exception $e) {
+            return redirect(route('facility.index'))->with('failed', 'Failed Update Facility!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $facility = Facility::where('id', $id)->first();
-
+    public function destroy(Facility $facility) : RedirectResponse {
         try {
-            $facility->delete();
-
-            return redirect(route('facility'))->with('success', 'Successfully Delete Facility!');
+            $this->facility->delete($facility);
+            return redirect(route('facility.index'))->with('success', 'Successfully Delete Facility!');
         } catch (\Throwable $th) {
-            return redirect(route('facility'))->with('failde', 'Failed Delete Facility!');
+            return redirect(route('facility.index'))->with('failed', 'Failed Delete Facility!');
         }
     }
 }
