5f46ab053ed97cc1486570c95b4df0e470b1bd04
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:15:30 +0800

    Progress: Update Clean Code Category Controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,67 +5,64 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
+use App\Repositories\CategoryRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
+    public function __construct(
+        protected readonly CategoryRepositories $category,
+    ) {}
+
     public function index() : View {
+        $categories = $this->category->findAllPaginate();
         return view('category.index', [
             'title' => 'Category Page',
-            'categories' => Category::orderBy('created_at', 'DESC')->paginate(10),
+            'categories' => $categories,
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $category = Category::where('id', $id)->first();
+    public function show(Category $category) : JsonResponse {
         try {
             return response()->json([
                 'status' => 'success',
-                'data' => $category,
+                'data' => $this->category->findById($category->id),
             ]);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Category with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Category with ID ' . $category->id,
+            ], 500);
         }
     }
 
     public function store(StoreCategoryRequest $request) : RedirectResponse {
         try {
-            Category::create($request->all());
-
-            return redirect(route('category'))->with('success', 'Successfully Add New Category!');
-        } catch (\Throwable $th) {
-            return redirect(route('category'))->with('failed', 'Failed Add New Category!');
+            $this->category->store($request->validated());
+            return redirect(route('category.index'))->with('success', 'Successfully Add New Category!');
+        } catch (\Exception $e) {  
+            logger($e->getMessage());
+            return redirect(route('category.index'))->with('failed', 'Failed Add New Category!');
         }
     }
 
-    public function update(UpdateCategoryRequest $request, $id) : RedirectResponse {
-        $category = Category::where('id', $id)->first();
-
-        try {
-            $category->update($request->all());
-
-            return redirect(route('category'))->with('success', 'Successfully Update Category!');
-
-        } catch (\Throwable $th) {
-            return redirect(route('Category'))->with('failed', 'Failed Update Category!');
+    public function update(UpdateCategoryRequest $request, Category $category) : RedirectResponse {
+        try {                     
+            $this->category->update($request->validated(), $category);
+            return redirect(route('category.index'))->with('success', 'Successfully Update Category!');
+        } catch (\Exception $e) {
+            return redirect(route('category.index'))->with('failed', 'Failed Update Category!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $category = Category::where('id', $id)->first();
-
+    public function destroy(Category $category) : RedirectResponse {
         try {
-            $category->delete();
-
-            return redirect(route('category'))->with('success', 'Successfully Delete Category!');
-        } catch (\Throwable $th) {
-            return redirect(route('category'))->with('failde', 'Failed Delete Category!');
+            $this->category->delete($category);
+            return redirect(route('category.index'))->with('success', 'Successfully Delete Category!');
+        } catch (\Exception $e) {            
+            return redirect(route('category.index'))->with('failed', 'Failed Delete Category!');
         }
     }
 }
