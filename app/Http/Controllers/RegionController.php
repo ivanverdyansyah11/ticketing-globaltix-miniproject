1e7d9098e210621be4a07202dafae3f100d08cc9
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:16:25 +0800

    Progress: Update Clean Code Region  Controller

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -6,68 +6,67 @@ use App\Http\Requests\StoreRegionRequest;
 use App\Http\Requests\UpdateRegionRequest;
 use App\Models\Language;
 use App\Models\Region;
+use App\Repositories\LanguageRepositories;
+use App\Repositories\RegionRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class RegionController extends Controller
 {
+    public function __construct(
+        protected readonly RegionRepositories $region,
+        protected readonly LanguageRepositories $language,
+    ) {}
+
     public function index() : View {
         return view('region.index', [
-            'title' => 'Region Page',
-            'regions' => Region::with(['language'])->orderBy('created_at', 'DESC')->paginate(10),
-            'languages' => Language::all(),
+            'title' => 'Language Page',
+            'regions' => $this->region->findAllPaginate(),
+            'languages' => $this->language->findAll(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $region = Region::with(['language'])->where('id', $id)->first();
-        $languages = Language::all();
+    public function show(Region $region) : JsonResponse {
         try {
             return response()->json([
                 'status' => 'success',
-                'data' => [$region, $languages],
+                'data' => $this->region->findById($region->id),
+                'languages' => $this->language->findAll(),
             ]);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Region with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Region with ID ' . $region->id,
+            ], 500);
         }
     }
 
     public function store(StoreRegionRequest $request) : RedirectResponse {
         try {
-            Region::create($request->all());
-
-            return redirect(route('region'))->with('success', 'Successfully Add New Region!');
-        } catch (\Throwable $th) {
-            return redirect(route('region'))->with('failed', 'Failed Add New Region!');
+            $this->region->store($request->validated());
+            return redirect(route('region.index'))->with('success', 'Successfully Add New Region!');
+        } catch (\Exception $e) {  
+            logger($e->getMessage());
+            return redirect(route('region.index'))->with('failed', 'Failed Add New Region!');
         }
     }
 
-    public function update(UpdateRegionRequest $request, $id) : RedirectResponse {
-        $region = Region::where('id', $id)->first();
-
-        try {
-            $region->update($request->all());
-
-            return redirect(route('region'))->with('success', 'Successfully Update Region!');
-
-        } catch (\Throwable $th) {
-            return redirect(route('region'))->with('failed', 'Failed Update Region!');
+    public function update(UpdateRegionRequest $request, Region $region) : RedirectResponse {
+        try {                     
+            $this->region->update($request->validated(), $region);
+            return redirect(route('region.index'))->with('success', 'Successfully Update Region!');
+        } catch (\Exception $e) {
+            return redirect(route('region.index'))->with('failed', 'Failed Update Region!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $region = Region::where('id', $id)->first();
-
+    public function destroy(Region $region) : RedirectResponse {
         try {
-            $region->delete();
-
-            return redirect(route('region'))->with('success', 'Successfully Delete Region!');
-        } catch (\Throwable $th) {
-            return redirect(route('region'))->with('failde', 'Failed Delete Region!');
+            $this->region->delete($region);
+            return redirect(route('region.index'))->with('success', 'Successfully Delete Region!');
+        } catch (\Exception $e) {            
+            return redirect(route('region.index'))->with('failed', 'Failed Delete Region!');
         }
     }
 }
