7f0f6d68fec3fd79defa4941483e5d4d819f29f0
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:16:18 +0800

    Progress: Update Clean Code Region Category Controller

--- app/Http/Controllers/RegionCategoryController.php
+++ app/Http/Controllers/RegionCategoryController.php
@@ -4,75 +4,74 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRegionCategoryRequest;
 use App\Http\Requests\UpdateRegionCategoryRequest;
-use App\Models\Category;
-use App\Models\Region;
 use App\Models\RegionCategory;
+use App\Repositories\CategoryRepositories;
+use App\Repositories\RegionCategoryRepositories;
+use App\Repositories\RegionRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class RegionCategoryController extends Controller
 {
+    public function __construct(
+        protected readonly RegionCategoryRepositories $regioncategory,
+        protected readonly RegionRepositories $region,
+        protected readonly CategoryRepositories $category,
+    ) {}
+
     public function index() : View {
         return view('region-category.index', [
             'title' => 'Region Category Page',
-            'region_categories' => RegionCategory::orderBy('created_at', 'DESC')->paginate(10),
-            'regions' => Region::all(),
-            'categories' => Category::all(),
+            'region_categories' => $this->regioncategory->findAllPaginate(),
+            'regions' => $this->region->findAll(),
+            'categories' => $this->category->findAll(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $regionCategory = RegionCategory::with(['region', 'category'])->where('id', $id)->first();
-        $categories = Category::all();
-        $regions = Region::all();
+    public function show(RegionCategory $regioncategory) : JsonResponse {
+        $regioncategory = $this->regioncategory->findById($regioncategory->id);
+        $regions = $this->region->findAll();
+        $categories = $this->category->findAll();
         try {
             return response()->json([
                 'status' => 'success',
-                'data' => [$regionCategory, $categories, $regions],
+                'data' => [$regioncategory, $categories, $regions],
             ]);
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Region Category with ID ' . $id,
-            ], 404);
+                'message' => 'Failed to get data for Region Category with ID ' . $regioncategory->id,
+            ], 500);
         }
     }
 
     public function store(StoreRegionCategoryRequest $request) : RedirectResponse {
         try {
-            $request['categories_id'] = implode(',', $request['categories_id']);
-            RegionCategory::create($request->all());
+            $this->regioncategory->store($request->validated());
 
-            return redirect(route('region_category'))->with('success', 'Successfully Add New Region Category!');
+            return redirect(route('regioncategory.index'))->with('success', 'Successfully Add New Region Category!');
         } catch (\Throwable $th) {
-            return redirect(route('region_category'))->with('failed', 'Failed Add New Region Category!');
+            return redirect(route('regioncategory.index'))->with('failed', 'Failed Add New Region Category!');
         }
     }
 
-    public function update(UpdateRegionCategoryRequest $request, $id) : RedirectResponse {
-        $regionCategory = RegionCategory::where('id', $id)->first();
-
+    public function update(UpdateRegionCategoryRequest $request, RegionCategory $regioncategory) : RedirectResponse {
         try {
-            $request['categories_id'] = implode(',', $request['categories_id']);
-            $regionCategory->update($request->all());
+            $this->regioncategory->update($request->validated(), $regioncategory);
 
-            return redirect(route('region_category'))->with('success', 'Successfully Update Region Category!');
-
+            return redirect(route('regioncategory.index'))->with('success', 'Successfully Update Region Category!');
         } catch (\Throwable $th) {
-            return redirect(route('region_category'))->with('failed', 'Failed Update Region Category!');
+            return redirect(route('regioncategory.index'))->with('failed', 'Failed Update Region Category!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $regionCategory = RegionCategory::where('id', $id)->first();
-
+    public function destroy(RegionCategory $regioncategory) : RedirectResponse {
         try {
-            $regionCategory->delete();
-
-            return redirect(route('region_category'))->with('success', 'Successfully Delete Region Category!');
-        } catch (\Throwable $th) {
-            return redirect(route('region_category'))->with('failde', 'Failed Delete Region Category!');
+            $this->regioncategory->delete($regioncategory);
+            return redirect(route('regioncategory.index'))->with('success', 'Successfully Delete Region Category!');
+        } catch (\Exception $e) {            
+            return redirect(route('regioncategory.index'))->with('failed', 'Failed Delete Region Category!');
         }
     }
 }
