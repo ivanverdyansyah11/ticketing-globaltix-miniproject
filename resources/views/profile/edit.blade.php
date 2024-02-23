167354da4729e8e7e9a3e29ca350a2f903f85761
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 19:32:01 +0800

    Progress: Finish Edit Page Profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -8,8 +8,12 @@
                 <div class="col-3">
                     <div class="card">
                         <div class="card-body px-3">
-                            @if (file_exists(public_path('assets/images/profile/' . $profile->user->image)))
-                                <img src="{{ asset('assets/images/profile/' . $profile->user->image) }}" alt="Profile Image" class="img-fluid img-preview" style="width: 100%; border-radius: 9999px;">
+                            @if (auth()->user()->role != 'super_admin')
+                                @if (file_exists(public_path('assets/images/profile/' . $profile->user->image)))
+                                    <img src="{{ asset('assets/images/profile/' . $profile->user->image) }}" alt="Profile Image" class="img-fluid img-preview rounded">
+                                @else
+                                    <img src="{{ asset('assets/images/profile/profile-not-found.jpg') }}" alt="Profile Image" class="img-fluid img-preview" style="width: 100%; border-radius: 9999px;">
+                                @endif
                             @else
                                 <img src="{{ asset('assets/images/profile/profile-not-found.jpg') }}" alt="Profile Image" class="img-fluid img-preview" style="width: 100%; border-radius: 9999px;">
                             @endif
@@ -30,61 +34,105 @@
                                 <div class="card-body px-3 px-md-4">
                                     <form action="{{ route('profile.update') }}" method="POST">
                                         @csrf
-                                        <div class="mb-3">
-                                            <label for="name" class="form-label">Name</label>
-                                            <input required name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" value="{{ $profile->name }}">
-                                            @error('name')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
+                                        @if (auth()->user()->role != 'super_admin')
+                                            <div class="mb-3">
+                                                <label for="name" class="form-label">Name</label>
+                                                <input required name="name" type="text" class="form-control @error('name') is-invalid @enderror" id="name" value="{{ $profile->name }}">
+                                                @error('name')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
-                                            @enderror
-                                        </div>
+                                        @endif
                                         <div class="mb-3">
                                             <label for="email" class="form-label">Email</label>
-                                            <input required name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ $profile->user->email }}">
+                                            <input required name="email" type="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ auth()->user()->role != 'super_admin' ? $profile->user->email : $profile->email }}">
                                             @error('email')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
                                             @enderror
                                         </div>
                                         <div class="mb-3">
                                             <label for="password" class="form-label">New Password</label>
                                             <input name="password" type="password" class="form-control @error('password') is-invalid @enderror" id="password">
                                             @error('password')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
                                             @enderror
                                         </div>
-                                        <div class="mb-3">
-                                            <label for="phone_number" class="form-label">Phone Number</label>
-                                            <input name="phone_number" type="number" class="form-control @error('phone_number') is-invalid @enderror" id="phone_number" value="{{ $profile->phone_number }}">
-                                            @error('phone_number')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
+                                        @if (auth()->user()->role != 'super_admin')
+                                            <div class="mb-3">
+                                                <label for="phone_number" class="form-label">Phone Number</label>
+                                                <input name="phone_number" type="number" class="form-control @error('phone_number') is-invalid @enderror" id="phone_number" value="{{ $profile->phone_number }}">
+                                                @error('phone_number')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
-                                            @enderror
-                                        </div>
-                                        <div class="mb-3">
-                                            <label for="place_of_birth" class="form-label">Place Of Birth</label>
-                                            <input required name="place_of_birth" type="text" class="form-control @error('place_of_birth') is-invalid @enderror" id="place_of_birth" value="{{ $profile->place_of_birth }}">
-                                            @error('place_of_birth')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
+                                            <div class="mb-3">
+                                                <label for="place_of_birth" class="form-label">Place Of Birth</label>
+                                                <input required name="place_of_birth" type="text" class="form-control @error('place_of_birth') is-invalid @enderror" id="place_of_birth" value="{{ $profile->place_of_birth }}">
+                                                @error('place_of_birth')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
-                                            @enderror
-                                        </div>
-                                        <div class="mb-4">
-                                            <label for="date_of_birth" class="form-label">Date Of Birth</label>
-                                            <input required name="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" id="date_of_birth" value="{{ $profile->date_of_birth }}">
-                                            @error('date_of_birth')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
+                                            <div class="mb-3">
+                                                <label for="date_of_birth" class="form-label">Date Of Birth</label>
+                                                <input required name="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" id="date_of_birth" value="{{ $profile->date_of_birth }}">
+                                                @error('date_of_birth')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
                                             </div>
-                                            @enderror
-                                        </div>
-                                        <div class="wrapper d-flex gap-2">
+                                        @endif
+                                        @if (auth()->user()->role == 'tourguide')
+                                            <div class="mb-3">
+                                                <label class="form-label">Language</label>
+                                                <div class="row row-cols-md-2 row-cols-md-3 row-cols-lg-4">
+                                                    @php
+                                                        $language_selected = explode(',', $profile->languages_id);
+                                                    @endphp
+                                                    @foreach ($languages as $language)
+                                                        @if (in_array($language->id, $language_selected))
+                                                            <div class="col mb-1">
+                                                                <input type="checkbox" name="languages_id[]" id="{{ $language->name }}" value="{{ $language->name }}" checked>
+                                                                <label for="{{ $language->name }}">{{ $language->name }}</label>
+                                                            </div>
+                                                        @else
+                                                            <div class="col">
+                                                                <input type="checkbox" name="languages_id[]" id="{{ $language->name }}" value="{{ $language->name }}">
+                                                                <label for="{{ $language->name }}">{{ $language->name }}</label>
+                                                            </div>
+                                                        @endif
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                        @endif
+                                        @if (auth()->user()->role == 'staff')
+                                            <div class="mb-3">
+                                                <label for="position" class="form-label">Position</label>
+                                                <input required name="position" type="text" class="form-control @error('position') is-invalid @enderror" id="position" value="{{ $profile->position }}">
+                                                @error('position')
+                                                    <div class="invalid-feedback">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </div>
+                                        @endif
+                                        @if (auth()->user()->role == 'customer')
+                                            <div class="mb-3">
+                                                <label for="address" class="form-label">Address</label>
+                                                <textarea required name="address" rows="4" class="form-control" id="address">{{ $profile->address }}</textarea>
+                                            </div>
+                                        @endif
+                                        <div class="wrapper d-flex gap-2 mt-3">
                                             <button type="submit" class="btn btn-primary">Save Changes</button>
                                             <a href="{{ route('profile') }}" class="btn btn-dark">Cancel Edit</a>
                                         </div>
