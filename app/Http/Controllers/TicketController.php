1507b0ce1c67dd209c108165e4cee9d28b3e6f38
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 08:16:41 +0800

    Progress: Update Clean Code Ticket Controller

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -7,26 +7,34 @@ use App\Http\Requests\UpdateTicketRequest;
 use App\Models\Ticket;
 use App\Models\TicketCategory;
 use App\Models\TouristSiteFacility;
+use App\Repositories\TicketCategoryRepositories;
+use App\Repositories\TicketRepositories;
+use App\Repositories\TouristSiteFacilityRepositories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class TicketController extends Controller
 {
+    public function __construct(
+        protected readonly TicketRepositories $ticket,
+        protected readonly TouristSiteFacilityRepositories $touristsitefacility,
+        protected readonly TicketCategoryRepositories $ticketcategory,
+    ) {}
+
     public function index() : View {
         return view('ticket.index', [
             'title' => 'Ticket  Page',
-            'tickets' => Ticket::with(['touristSiteFacility.touristsite', 'category'])->orderBy('created_at', 'DESC')->paginate(10),
-            'tourist_site_facilities' => TouristSiteFacility::with(['touristsite'])->get(),
-            'categories' => TicketCategory::all(),
+            'tickets' => $this->ticket->findAllPaginate(),
+            'tourist_site_facilities' => $this->touristsitefacility->findAll(),
+            'categories' => $this->ticketcategory->findAll(),
         ]);
     }
 
-    public function detail($id) : JsonResponse {
-        $ticket = Ticket::with(['touristSiteFacility.touristsite', 'category'])->where('id', $id)->first();
-        $tourist_site_facilities = TouristSiteFacility::with(['touristsite'])->get();
-        $categories = TicketCategory::all();
+    public function show(Ticket $ticket) : JsonResponse {
+        $ticket = $this->ticket->findById($ticket->id);
+        $tourist_site_facilities = $this->touristsitefacility->findAll();
+        $categories = $this->ticketcategory->findAll();
         try {
             return response()->json([
                 'status' => 'success',
@@ -35,7 +43,7 @@ class TicketController extends Controller
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Failed to get data for Ticket  with ID ' . $id,
+                'message' => 'Failed to get data for Ticket  with ID ' . $ticket->id,
             ], 404);
         }
     }
@@ -44,41 +52,34 @@ class TicketController extends Controller
         try {
             $ticketExist = Ticket::where('tourist_site_facilities_id', $request->tourist_site_facilities_id)->where('ticket_categories_id', $request->ticket_categories_id)->first();
             if ($ticketExist) {
-                return redirect(route('ticket'))->with('failed', 'Ticket is Already Exist!');
+                return redirect(route('ticket.index'))->with('failed', 'Ticket is Already Exist!');
             }
-            Ticket::create($request->all());
-
-            return redirect(route('ticket'))->with('success', 'Successfully Add New Ticket!');
+            $this->ticket->store($request->validated());
+            return redirect(route('ticket.index'))->with('success', 'Successfully Add New Ticket!');
         } catch (\Throwable $th) {
-            return redirect(route('ticket'))->with('failed', 'Failed Add New Ticket!');
+            return redirect(route('ticket.index'))->with('failed', 'Failed Add New Ticket!');
         }
     }
 
-    public function update(UpdateTicketRequest $request, $id) : RedirectResponse {
-        $ticket = Ticket::where('id', $id)->first();
-
+    public function update(UpdateTicketRequest $request, Ticket $ticket) : RedirectResponse {
         try {
             $ticketExist = Ticket::where('tourist_site_facilities_id', $request->tourist_site_facilities_id)->where('ticket_categories_id', $request->ticket_categories_id)->first();
             if ($ticket->id == $ticketExist->id) {
-                $ticket->update($request->all());
-                return redirect(route('ticket'))->with('success', 'Successfully Update Ticket!');
+                $this->ticket->update($request->validated(), $ticket);
+                return redirect(route('ticket.index'))->with('success', 'Successfully Update Ticket!');
             }
-
-            return redirect(route('ticket'))->with('failed', 'Cannot Update Ticket if The Ticket Category Already Exists!');
+            return redirect(route('ticket.index'))->with('failed', 'Cannot Update Ticket if The Ticket Category Already Exists!');
         } catch (\Throwable $th) {
-            return redirect(route('ticket'))->with('failed', 'Failed Update Ticket!');
+            return redirect(route('ticket.index'))->with('failed', 'Failed Update Ticket!');
         }
     }
 
-    public function delete($id) : RedirectResponse {
-        $ticket = Ticket::where('id', $id)->first();
-
+    public function destroy(Ticket $ticket) : RedirectResponse {
         try {
-            $ticket->delete();
-
-            return redirect(route('ticket'))->with('success', 'Successfully Delete Ticket!');
+            $this->ticket->delete($ticket);
+            return redirect(route('ticket.index'))->with('success', 'Successfully Delete Ticket!');
         } catch (\Throwable $th) {
-            return redirect(route('ticket'))->with('failde', 'Failed Delete Ticket!');
+            return redirect(route('ticket.index'))->with('failde', 'Failed Delete Ticket!');
         }
     }
 }
